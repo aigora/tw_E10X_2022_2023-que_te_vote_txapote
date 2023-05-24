6e0d71ae6ef87e73f7a9107493a8432363728981
teresavillajos <130778202+teresavillajos@users.noreply.github.com>
2023-05-24 23:27:07 +0200

    Add files via upload

---
+++ ejercicio3temperatura.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+ float tempc, tempf;
+ printf ("Escriba una temperatura en grados centigrados:\n");
+ scanf ("%f", &tempc);
+
+ tempf = (( 9.0 / 5.0 ) * tempc + 32);
+ printf ("La temperatura en grados centigrados equivale a %.2f en grados Fahrenheit\n", tempf);
+
+
+
+
+
+  return 0;
+}
